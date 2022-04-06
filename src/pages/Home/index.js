64d796eef01a7e0ac215b87af301f0ec37d8201f
Nairon Carneiro <61030718+NaironCarneiro@users.noreply.github.com>
2022-04-05 22:25:42 -0300

    add styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,56 @@
+import { Container, MovieList, Movie } from "./styles";
+
 function Home(){
+
+    const movies = [
+        {
+        id:1,
+        title:'Spider Man',
+        image_url:'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+        id:2,
+        title:'Venum',
+        image_url:'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+
+        },
+        {
+        id:3,
+        title:'Doctor',
+        image_url:'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+
+        },
+        {
+        id:1,
+        title:'Spider Man',
+        image_url:'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+        },
+        {
+        id:2,
+        title:'Venum',
+        image_url:'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+
+        },
+        {
+        id:3,
+        title:'Doctor',
+        image_url:'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+        }
+]
+
     return(
-        <h1>Olá</h1>
+        <Container>
+        <h1>Movies</h1>
+        <MovieList>
+            {movies.map(movie => {
+                return(
+                    <Movie key={movie.id}>
+                    <a href="https://github.com/NaironCarneiro"><img src={movie.image_url} alt={movie.title} /></a>
+                    <span>{movie.title}</span>
+                    </Movie>
+                )
+            })}
+        </MovieList>
+        </Container>  
     )
-
-
 }export default Home;
